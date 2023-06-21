a542d4f7dbbfddcb55366a08f2e4530a56caafe2
us2002 <ujjawal.soni1507@gmail.com>
2023-06-22 04:27:59 +0530

    Basic GUI

--- GUI/script.js
+++ GUI/script.js
@@ -1,33 +1,56 @@
+var divs = ["div1", "div2", "div3", "div4", "div5"];
+var visibleDivId = null;
+function divVisibility(divId) {
+  if (visibleDivId == divId) {
+    visibleDivId = null;
+  } else {
+    visibleDivId = divId;
+  }
+  hideNonVisibleDivs();
+}
+function hideNonVisibleDivs() {
+  var i, divId, div;
+  for (i = 0; i < divs.length; i++) {
+    divId = divs[i];
+    div = document.getElementById(divId);
+    if (visibleDivId == divId) {
+      div.style.display = "block";
+    } else {
+      div.style.display = "none";
+    }
+  }
+}
+
 const form = document.querySelector("form"),
-    fileInput = document.querySelector(".file-input"),
-    progressArea = document.querySelector(".progress-area"),
-    uploadedArea = document.querySelector(".uploaded-area");
+  fileInput = document.querySelector(".file-input"),
+  progressArea = document.querySelector(".progress-area"),
+  uploadedArea = document.querySelector(".uploaded-area");
 
 form.addEventListener("click", () => {
-    fileInput.click();
+  fileInput.click();
 });
 
 fileInput.onchange = ({ target }) => {
-    let file = target.files[0];
-    if (file) {
-        let fileName = file.name;
-        if (fileName.length >= 12) {
-            let splitName = fileName.split('.');
-            fileName = splitName[0].substring(0, 13) + "... ." + splitName[1];
-        }
-        uploadFile(fileName);
+  let file = target.files[0];
+  if (file) {
+    let fileName = file.name;
+    if (fileName.length >= 12) {
+      let splitName = fileName.split('.');
+      fileName = splitName[0].substring(0, 13) + "... ." + splitName[1];
     }
+    uploadFile(fileName);
+  }
 }
 
 function uploadFile(name) {
-    let xhr = new XMLHttpRequest();
-    xhr.open("POST", "php/upload.php");
-    xhr.upload.addEventListener("progress", ({ loaded, total }) => {
-        let fileLoaded = Math.floor((loaded / total) * 100);
-        let fileTotal = Math.floor(total / 1000);
-        let fileSize;
-        (fileTotal < 1024) ? fileSize = fileTotal + " KB" : fileSize = (loaded / (1024 * 1024)).toFixed(2) + " MB";
-        let progressHTML = `<li class="row">
+  let xhr = new XMLHttpRequest();
+  xhr.open("POST", "php/upload.php");
+  xhr.upload.addEventListener("progress", ({ loaded, total }) => {
+    let fileLoaded = Math.floor((loaded / total) * 100);
+    let fileTotal = Math.floor(total / 1000);
+    let fileSize;
+    (fileTotal < 1024) ? fileSize = fileTotal + " KB" : fileSize = (loaded / (1024 * 1024)).toFixed(2) + " MB";
+    let progressHTML = `<li class="row">
                           <i class="fas fa-file-alt"></i>
                           <div class="content">
                             <div class="details">
@@ -39,11 +62,11 @@ function uploadFile(name) {
                             </div>
                           </div>
                         </li>`;
-        uploadedArea.classList.add("onprogress");
-        progressArea.innerHTML = progressHTML;
-        if (loaded == total) {
-            progressArea.innerHTML = "";
-            let uploadedHTML = `<li class="row">
+    uploadedArea.classList.add("onprogress");
+    progressArea.innerHTML = progressHTML;
+    if (loaded == total) {
+      progressArea.innerHTML = "";
+      let uploadedHTML = `<li class="row">
                             <div class="content upload">
                               <i class="fas fa-file-alt"></i>
                               <div class="details">
@@ -53,10 +76,10 @@ function uploadFile(name) {
                             </div>
                             <i class="fas fa-check"></i>
                           </li>`;
-            uploadedArea.classList.remove("onprogress");
-            uploadedArea.insertAdjacentHTML("afterbegin", uploadedHTML);
-        }
-    });
-    let data = new FormData(form);
-    xhr.send(data);
+      uploadedArea.classList.remove("onprogress");
+      uploadedArea.insertAdjacentHTML("afterbegin", uploadedHTML);
+    }
+  });
+  let data = new FormData(form);
+  xhr.send(data);
 }
